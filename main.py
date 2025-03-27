618b3d3795d946df49974d25611fae070ae2be88
braydencurtis <braydencurtis@gmail.com>
2025-03-27 15:01:45 -0700

    Replaced book path with sys arg

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from stats import count_characters
 from stats import sorted_dict
 import sys
 
-book = "books/frankenstein.txt"
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book = sys.argv[1]
 
 print("============ BOOKBOT ============")
 print(f"Analyzing book found at {book}...")
