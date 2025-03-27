298f88683e9630caa4b08bf34f001e0e042a50a6
braydencurtis <braydencurtis@gmail.com>
2025-03-27 15:14:51 -0700

    Add comments

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from stats import count_characters
 from stats import sorted_dict
 import sys
 
+# if the user does not provide a book file, print usage message and exit
 if len(sys.argv) != 2:
     print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
 
+# assign file path from sys to book variable
 book = sys.argv[1]
 
+# Report the book file name and the number of words and characters in the book
+# and the frequency of each character
 print("============ BOOKBOT ============")
 print(f"Analyzing book found at {book}...")
 print("----------- Word Count ----------")
--- stats.py
+++ stats.py
@@ -1,20 +1,24 @@
 from collections import Counter
 
+# Convert book.txt file into a workable string
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
+# count the number of words in the book file and return an Int
 def count_words(path_to_file):
     word_list = get_book_text(path_to_file).split()
     word_count = len(word_list)
     return word_count
 
+# count the number of characters in the book file, and return a dictionary
 def count_characters(path_to_file):
     lower_case_contents = get_book_text(path_to_file).lower()
     char_counts = Counter(lower_case_contents)
     return char_counts
 
+# sort the dictionary by value in descending order
 def sorted_dict(dictionary):
     sorted_items = sorted(dictionary.items(), key=lambda item: item[1], reverse=True)
     sorted_dictionary = dict(sorted_items)
