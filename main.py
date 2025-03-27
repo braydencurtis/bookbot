fee2bc8cf407cc038da061034e5319a25da248d9
Brayden Curtis <brayden@Ovaltine-Jenkins.local>
2025-03-26 22:57:51 -0700

    adding books

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from stats import count_words
+from stats import count_characters
+from stats import sorted_list
 
-print(f"{count_words("books/frankenstein.txt")} words found in the document")
+book = "books/frankenstein.txt"
+
+print(f"{count_words(book)} words found in the document")
+print(count_characters(book))
+
+print(sorted_list(count_characters(book)))
--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
@@ -7,3 +9,17 @@ def count_words(path_to_file):
     word_list = get_book_text(path_to_file).split()
     word_count = len(word_list)
     return word_count
+
+def count_characters(path_to_file):
+    lower_case_contents = get_book_text(path_to_file).lower()
+    char_counts = Counter(lower_case_contents)
+    return char_counts
+
+
+def sort_on(dict):
+    return dict["num"]
+
+def sorted_list(list):
+    sorted = list.sort(reverse=True, key=sort_on)
+    print(sorted)
+
